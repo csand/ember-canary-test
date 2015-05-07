fdfb124a3ce77da46a074576b8571a12040c25d1
csand <sam@csand.net>
2015-05-07 10:12:58 -0400

    Add stress-test, update README

--- app/components/stress-test.js
+++ app/components/stress-test.js
@@ -1,5 +1,33 @@
 import Ember from 'ember';
 
+let computed = Ember.computed;
+
 export default Ember.Component.extend({
-  tagName: 'svg'
+  width: window.innerWidth,
+  height: window.innerHeight,
+  children: computed(function() {
+    let children = [];
+    for (let i = 0; i < 10000; i++) {
+      children.push(Ember.Object.create({
+        id: i,
+        color: '#000000',
+        x: window.innerWidth * Math.random(),
+        y: window.innerHeight * Math.random()
+      }));
+    }
+    return children;
+  }),
+  didInsertElement() {
+    function random255() {
+      return Math.floor(Math.random() * 255);
+    }
+    function randomRGB() {
+      return `rgb(${random255()}, ${random255()} , ${random255()})`;
+    }
+    setInterval(() => {
+      this.get('children').map(child => {
+        child.set('color', randomRGB());
+      });
+    }, 10);
+  }
 });
